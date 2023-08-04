296f1a85fe2a55fa852323b758eb118582b2c76a
Sirmauricio10000 <mauricio1907_5@outlook.com>
2023-08-03 22:44:04 -0500

    try catch added to link_clicked function

--- myapp/views.py
+++ myapp/views.py
@@ -9,18 +9,16 @@ def index(request):
 
 
 def link_clicked(request, link_url):
-    # Guardar la dirección IP y la fecha en la base de datos
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
 
-    # Guardar la dirección IP y la fecha en la base de datos
-    Link.objects.create(ip_address=ip, date_clicked=timezone.now())
-
-    # Realizar la redirección al enlace de GitHub
-    return redirect(link_url)
+    try:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        Link.objects.create(ip_address=ip, date_clicked=timezone.now())
+    finally:
+        return redirect(link_url)
 
 
 
